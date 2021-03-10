7253bf44cc99258ef17b2ca2ecd18b8c86a6b3e5
Muhammad Khoirul Wafa <muhammad.wafa@modalrakyat.id>
2021-03-10 14:58:55 +0700

    ✨ Add title section, cleaning code, and remove some unused funct

--- src/screens/Timer/index.js
+++ src/screens/Timer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, Text, StatusBar, TouchableOpacity, Vibration} from 'react-native';
-import {BottomNav} from '../../components';
+import {BottomNav, Title, Circle, CategoryChip} from '../../components';
 import CountDown from 'react-native-countdown-component';
 import * as Progress from 'react-native-progress';
 import {widthPercentageToDP} from 'react-native-responsive-screen';
@@ -48,7 +48,11 @@ const Timer = ({navigation}) => {
     playSong();
     setStart(false);
   };
-
+  const stopAll = () => {
+    SoundPlayer.stop();
+    Vibration.cancel();
+    setNum(0.0);
+  };
   const renderRemainingTime = () => {
     return (
       <CountDown
@@ -67,54 +71,78 @@ const Timer = ({navigation}) => {
     );
   };
   const wrap = {flex: 1, justifyContent: 'center', alignItems: 'center'};
+  const butt = (backgroundColor) => ({
+    backgroundColor: backgroundColor,
+    width: widthPercentageToDP('80'),
+    height: 50,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  });
+  const head = {
+    margin: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  };
+  const subtitle = {
+    flexDirection: 'row',
+    marginRight: 5,
+    alignItems: 'center',
+  };
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <View style={wrap}>
-        <Text>Timer</Text>
-        <Gap height={30} />
-        <Progress.Circle
-          progress={num}
-          size={widthPercentageToDP('46.5')}
-          formatText={() => renderRemainingTime()}
-          thickness={10}
-          showsText
-          strokeCap="round"
-        />
-        <Gap height={30} />
-        {num === 1 ? (
+      <View style={{justifyContent: 'space-between', flex: 1}}>
+        <View style={head}>
+          <View>
+            <Title size={20}>Rasion Project</Title>
+            <View style={subtitle}>
+              <Circle width={15} />
+              <Title family="Rubik-Regular" size={15}>
+                Ui Design
+              </Title>
+            </View>
+          </View>
+          <View>
+            <CategoryChip>
+              <Title size={11} color="#FD5B71">
+                Work
+              </Title>
+            </CategoryChip>
+          </View>
+        </View>
+        <Gap height={70} />
+        <View style={wrap}>
+          <Progress.Circle
+            progress={num}
+            size={widthPercentageToDP('46.5')}
+            formatText={() => renderRemainingTime()}
+            thickness={10}
+            showsText
+            strokeCap="round"
+            color="#9B51E0"
+          />
+        </View>
+        <View
+          style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
           <TouchableOpacity
             activeOpacity={0.7}
-            onPress={() => {
-              SoundPlayer.stop();
-              Vibration.cancel();
-              setNum(0.0);
-            }}
-            style={{
-              backgroundColor: 'salmon',
-              width: 100,
-              height: 40,
-              borderRadius: 20,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text>STOP</Text>
+            onPress={() => (num === 1 ? stopAll() : setStart(!start))}
+            style={butt('#E9E9FF')}>
+            <Title size={15}>
+              {num === 1 ? 'STOP' : start ? 'PAUSE' : 'START'}
+            </Title>
           </TouchableOpacity>
-        ) : (
+          <Gap height={20} />
           <TouchableOpacity
             activeOpacity={0.7}
-            onPress={() => setStart(!start)}
-            style={{
-              backgroundColor: 'salmon',
-              width: 100,
-              height: 40,
-              borderRadius: 20,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text>{start ? 'PAUSE' : 'START'}</Text>
+            onPress={() => (num === 1 ? stopAll() : setStart(!start))}
+            style={butt('transparent')}>
+            <Title size={15}>QUIT</Title>
           </TouchableOpacity>
-        )}
+          <Gap height={30} />
+        </View>
       </View>
       <BottomNav active="Timer" navigation={navigation} />
     </>
